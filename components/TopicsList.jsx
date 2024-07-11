d4c7b03da919d0c384ab51dfab0fc568e0fb40cf
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-07-11 19:08:59 +0300

    styling the TopicsList

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,18 +1,23 @@
+import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList(){
-    return(
-        <>
-            <div>
-                <div>
-                    <h2>Topic Title</h2>
-                    <div>Topic description</div>
-                </div>
+export default function TopicsList() {
+  return (
+    <>
+      <div className="flex justify-between items-start gap-5 p-4 border border-slate-300 my-3">
+        <div>
+          <h2 className="font-bold text-2xl">Topic Title</h2>
+          <div>Topic description</div>
+        </div>
 
-                <div>
-                    <RemoveBtn/>
-                </div>
-            </div>
-        </>
-    )
+        <div className="flex  gap-2">
+          <RemoveBtn />
+          <Link href={"/editTopic/123"}>
+            <HiPencilAlt className="pencilIcon" size={24}/>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
