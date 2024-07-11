348f30c852c41aada40c146c9db6834db457b476
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-07-12 00:57:16 +0300

    creating functions to connect to backend

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,9 +1,25 @@
-import {HiOutlineTrash} from "react-icons/hi"
+"use client";
 
-export default function RemoveBtn(){
-    return(
-        <button>
-            <HiOutlineTrash size={24} color="red"/>
-        </button>
-    )
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+export default function RemoveBtn({ id }) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure?");
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
+
+  return (
+    <button onClick={removeTopic}>
+      <HiOutlineTrash size={24} color="red" />
+    </button>
+  );
 }
